327bb64fd2d3342654b0c5ee1195683282030635
Unknown <readn@icloud.com>
2017-09-03 16:55:40 -0400

    Argument parser added.

--- NBtrain.py
+++ NBtrain.py
@@ -6,3 +6,20 @@ Created on Sun Sep  3 16:28:33 2017
 @author: Nuoyu Li (nuoyul@andrew.cmu.edu)
 """
 
+import sys
+
+def readcommandline():
+    try:
+        inputfile = open(sys.argv[1],'r')
+        return inputfile
+    except Exception as e:
+        print('invalid file directory, try again')
+        print('usage: python NBtrain.py <inputfile>')
+        sys.exit(2)
+
+if __name__ == '__main__':
+    openfile = readcommandline()
+    for line in openfile:
+        print(line.strip())
+        
+    
